f4d1622be5e44f6eb06076ee8a229a68edfc6919
iaditi <aditipython@gmail.com>
2024-11-30 16:41:11 +0530

    changes added

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,41 +1,54 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context";
 
 export default function Navbar() {
+  const { searchParam, setSearchParam, handleSubmit } =
+    useContext(GlobalContext);
+
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5">
+      {/* App Title */}
       <h2 className="text-2xl font-semibold">FoodRecipe</h2>
-      <form>
+
+      {/* Search Form */}
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
+          value={searchParam}
+          onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Enter Items..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
         />
       </form>
+
+      {/* Navigation Links */}
       <ul className="flex gap-5">
         <li>
           <NavLink
-            to={"/"}
+            to="/"
             className="text-black hover:text-gray-700 duration-300"
           >
-            {" "}
             Home
           </NavLink>
+        </li>
+        <li>
           <NavLink
-            to={"/favourites"}
+            to="/favourites"
             className="text-black hover:text-gray-700 duration-300"
           >
-            {" "}
             Favourites
           </NavLink>
         </li>
-        <NavLink
-          to={"/recipe-item/:id"}
-          className="text-black hover:text-gray-700 duration-300"
-        >
-          {" "}
-          Recipe
-        </NavLink>
+        <li>
+          <NavLink
+            to="/recipe-item/:id"
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Recipe
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,30 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
-export default function Home(){
-    return(
+export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader"></div>
+        <p className="text-center text-lg mt-4">Loading... Please wait!</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-4">
+      {recipeList?.length > 0 ? (
+        recipeList.map((item, index) => <RecipeItem key={index} item={item} />)
+      ) : (
         <div>
-            HOME
+          <p className="lg:text-4xl text-xl text-center text-gray-500 font-bold">
+            Nothing to show. Please search something.
+          </p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
