784c0be4c724d7ba711d7cc55081e7cacc8030df
iaditi <aditipython@gmail.com>
2024-11-30 18:19:04 +0530

    changes added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,23 +5,16 @@ import RecipeItem from "../../components/recipe-item";
 export default function Home() {
   const { recipeList, loading } = useContext(GlobalContext);
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="loader"></div>
-        <p className="text-center text-lg mt-4">Loading... Please wait!</p>
-      </div>
-    );
-  }
+  if (loading) return <div>Loading...Please wait!</div>;
 
   return (
-    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-4">
-      {recipeList?.length > 0 ? (
-        recipeList.map((item, index) => <RecipeItem key={index} item={item} />)
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
       ) : (
         <div>
-          <p className="lg:text-4xl text-xl text-center text-gray-500 font-bold">
-            Nothing to show. Please search something.
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
           </p>
         </div>
       )}
